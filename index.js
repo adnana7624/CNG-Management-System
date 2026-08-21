7d37a019783c91d5bf808401d16af3cdb7998b85
adnana7624 <adnanahmad8341@gmail.com>
2026-08-21 23:30:21 +0500

    add sale model adn contorller

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express, { json, urlencoded } from "express";
 import dotenv from "dotenv";
 import connectDB from "./src/config/db.js";
-import router from "./src/routes/userRoutes.js";
 
+// import routes
+import authRoutes from "./src/routes/authRoutes.js";
+import saleRoutes from "./src/routes/saleRoutes.js";
+import adminRoutes from "./src/routes/adminRoutes.js";
 
 const app = express();
+
 dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use("/api/v1/auth",router)
+
+
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/admins",adminRoutes)
+app.use("/api/v1/sales",saleRoutes);
 
 app.get("/",()=>{
     console.log("app runnig succesfuly")
