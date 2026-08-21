a18ca18732bc6c7c20ecfec8324042619ff0c449
Matif321 <matifkhan6038@gmail.com>
2026-08-21 16:20:01 +0500

    controller changing

--- src/controllers/saleController.js
+++ src/controllers/saleController.js
@@ -1,24 +1,27 @@
-import {Sale} from "../models/saleModel.js";
+import { Sale } from "../models/saleModel.js";
 
-export const createSale = async(req , res)=>{
+export const createSale = async (req, res) => {
     try {
-        const {date , cngVolume , amount , paymentMethod , notes} = req.body;
-        if(!date || !cngVolume || !amount ||!paymentMethod){
-            return res.status(400).json({message : "all fields are required"})
+        const { date, cngVolume, amount, paymentMethod, notes } = req.body;
+        if (!date || !cngVolume || !amount || !paymentMethod) {
+            return res.status(400).json({ message: "all fields are required" })
         }
 
         const adminId = req.user.id;
 
-        if(Number(cngVolume) <=0){
-            return res.status(400).json({message : "cng volume must be greater than 0"})
+        if (Number(cngVolume) <= 0) {
+            return res.status(400).json({ message: "cng volume must be greater than 0" })
+        }
+        if (Number(amount) <= 0) {
+            return res.status(400).json({ message: "cng voulme bust be fucing" })
         }
 
-        
+
     } catch (error) {
         return res.status(500).json({
-            success : false,
-            message : "error during add sale ",
-            message : error.message
+            success: false,
+            message: "error during add sale ",
+            message: error.message
         })
     }
 }
